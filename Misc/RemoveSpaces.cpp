c80f7e72292be5288bbee4f82000c855071e4cf8
Ahan Jain (PC) <ahanjain@gmail.com>
2024-02-29 21:15:32 +0530

    Create RemoveSpaces.cpp

---
+++ Misc/RemoveSpaces.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void space_remover(string &s)
+
+{
+    int spaceCount = 0;
+    int i = 0;
+
+    while (i + spaceCount < s.length())
+
+    {
+        if (s[i + spaceCount] != ' ')
+
+        {
+            swap(s[i], s[i + spaceCount]);
+            i++;
+        }
+
+        else
+
+        {
+            spaceCount++;
+        }
+    }
+}
+
+int main()
+
+{
+    string s = "aa bb cc d    e f ggg h h";
+    space_remover(s);
+
+    cout << s;
+
+    return 0;
+}
