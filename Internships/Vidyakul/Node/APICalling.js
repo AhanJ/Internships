3f3c9229e11778210029d25c092bb300d1d68cbb
Ahan Jain (MBA) <110718911+AhanJ@users.noreply.github.com>
2024-03-01 09:40:25 +0530

    Update APICalling.js

--- Internships/Vidyakul/Node/APICalling.js
+++ Internships/Vidyakul/Node/APICalling.js
@@ -1,35 +1,36 @@
-const axios = require('axios')
+const axios = require("axios");
 
-axios.get('https://catfact.ninja/fact').then(function (response) { // Method 1
-    
-    console.log("Fact 1: " + response.data.fact)
+axios
+  .get("https://catfact.ninja/fact")
+  .then(function (response) {
+    // Method 1
 
-  }).catch(function (error) {
-    
-    console.log(error)
-
-})
+    console.log("Fact 1: " + response.data.fact);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
 const getFact2 = async () => {
-    
-    const apiResponse = await axios.get('https://catfact.ninja/fact') // Method 2
-    console.log('Fact 2: '+ apiResponse.data.fact);
-  }
+  const apiResponse = await axios.get("https://catfact.ninja/fact"); // Method 2
+  console.log("Fact 2: " + apiResponse.data.fact);
+};
 
-getFact2()
+getFact2();
 
-const getFact3 = async () => { // Method 3
-    
-    const apiResponse = await axios({
-        
-        method: 'GET',
-        url: 'https://catfact.ninja/fact'
-    })
-    
-    console.log('Fact 3: '+ apiResponse.data.fact);
-  }
+const getFact3 = async () => {
+  // Method 3
 
-getFact3()
+  const apiResponse = await axios({
+    method: "GET",
+    url: "https://catfact.ninja/fact",
+  });
 
-// here the order of facts is random since we are using asynchronous functions since all such functions work indepnedent
-// of each other and their results are used as and when available
+  console.log("Fact 3: " + apiResponse.data.fact);
+};
+
+getFact3();
+
+// here the order of facts is random since we are using asynchronous functions
+// since all such functions work indepnedent of each other
+// their results are used as and when available
