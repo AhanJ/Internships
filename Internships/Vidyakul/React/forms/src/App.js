7deae638f866e250ded5fa864f8402d99085996c
Ahan Jain (MBA) <110718911+AhanJ@users.noreply.github.com>
2024-03-02 15:37:12 +0530

    Updated forms

--- Internships/Vidyakul/React/forms/src/App.js
+++ Internships/Vidyakul/React/forms/src/App.js
@@ -1,10 +1,16 @@
 import LoginForm from "./components/LoginForm";
+import Button from "./components/Button";
 import "./App.css";
 
 function App() {
   return (
-    <div class="h-dvh bg-slate-400">
-      <LoginForm />
+    <div class="h-dvh bg-slate-400 flex flex-col items-center justify-center">
+      <div>
+        <LoginForm />
+      </div>
+      <div>
+        <Button />
+      </div>
     </div>
   );
 }
