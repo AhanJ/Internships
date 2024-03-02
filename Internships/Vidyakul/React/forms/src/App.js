67a16759e1b28d564536f792b0ce28000ccecd56
Ahan Jain (MBA) <110718911+AhanJ@users.noreply.github.com>
2024-03-02 15:20:42 +0530

    forms

---
+++ Internships/Vidyakul/React/forms/src/App.js
@@ -0,0 +1,12 @@
+import LoginForm from "./components/LoginForm";
+import "./App.css";
+
+function App() {
+  return (
+    <div class="h-dvh bg-slate-400">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
