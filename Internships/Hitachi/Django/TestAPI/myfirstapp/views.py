0181a8e306a0057db047e79793ee172a2fd2c74d
Ahan Jain (PC) <ahanjain@gmail.com>
2024-05-18 13:53:34 +0530

    Update views.py

--- Internships/Hitachi/Django/TestAPI/myfirstapp/views.py
+++ Internships/Hitachi/Django/TestAPI/myfirstapp/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # a view function takes in a request and returns a response
 # a way to handle requests
 
 
+# returning http response
 def hello_world(request):
     return HttpResponse("Hello World")
+
+
+# returning markup using render function
+# automatically looks into the templates folder so no need to manually specify directory
+def etowerinfo(request):
+    return render(request, "etower.html")
