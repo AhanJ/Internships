ea9dd414ede143f0ee79c689539b6a978f1d9c86
Ahan Jain (PC) <ahanjain@gmail.com>
2024-05-24 18:40:34 +0530

    Update views.py

--- Internships/Hitachi/Django/TestAPI/crudAPI/views.py
+++ Internships/Hitachi/Django/TestAPI/crudAPI/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from myfirstapp.models import Employee
 from .serializers import EmployeeSerializer
 
@@ -33,18 +36,50 @@ def welcome(request):
 
 
 # Deserialised output: Employee object Employee object Employee object Employee object
+# handling request differently if it contains query parameters
+@api_view(["GET"])
+def readData(request):
+    if request.GET:
+        input = request.GET.get("fname")
+        data = Employee.objects.get(first_name=input)
+        # input = request.GET.dict()
+        # input = request.GET.items()
+        # return Response(input)
+        serializer = EmployeeSerializer(data, many=False)
 
-# After serialisation oup
+    else:
+        data = Employee.objects.all()
+        serializer = EmployeeSerializer(data, many=True)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(["POST"])
+def createData(request):
+    serializer = EmployeeSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(status=status.HTTP_201_CREATED)
+
+
+@api_view(["DELETE"])
+def deleteData(request, id):
+    data = Employee.objects.get(pk=id)
+    serializer = EmployeeSerializer(data, many=False)
+    data.delete()
+    return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(["PUT"])
+def updateData(request):
+    serializer = EmployeeSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET"])
-def getAll(request):
-    data = Employee.objects.all()
-    serializer = EmployeeSerializer(data, many=True)
-    return Response(serializer.data)
-
-
-@api_view(["GET"])
-def getOne(request):
-    input = request.GET.get("fname", "lname")
-    return Response(input)
+def temp(request):
+    print(request.body)
+    return Response(request.data)
