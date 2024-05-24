02618f03c6751577bf61f46f3326233670bd1913
Ahan Jain (PC) <ahanjain@gmail.com>
2024-05-24 18:40:32 +0530

    Update urls.py

--- Internships/Hitachi/Django/TestAPI/crudAPI/urls.py
+++ Internships/Hitachi/Django/TestAPI/crudAPI/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.welcome),
-    path("read/", views.getAll),
-    path("temp/", views.getOne),
+    path("read/", views.readData),
+    path("create/", views.createData),
+    path("delete/<int:id>", views.deleteData),
+    path("update/", views.updateData),
+    path("temp/", views.temp),
 ]
