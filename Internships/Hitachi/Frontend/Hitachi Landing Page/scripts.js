a627d1bfccd46ac99eb89b0ebfbc630146a37100
Ahan Jain (MBA) <110718911+AhanJ@users.noreply.github.com>
2024-06-13 15:49:44 +0530

    Updated Hitachi Landing Page

--- Internships/Hitachi/Frontend/Hitachi Landing Page/scripts.js
+++ Internships/Hitachi/Frontend/Hitachi Landing Page/scripts.js
@@ -2,12 +2,14 @@
 const navMenu = document.getElementById("nav-menu");
 const navOpen = document.getElementById("nav-open");
 const navClose = document.getElementById("nav-close");
+const body = document;
 
 // Show Menu
 if (navOpen) {
   navOpen.addEventListener("click", () => {
     navMenu.classList.add("show-menu");
     console.log("Done");
+    document.body.style.overflow = "hidden";
   });
 }
 
@@ -15,6 +17,7 @@ if (navOpen) {
 if (navClose) {
   navClose.addEventListener("click", () => {
     navMenu.classList.remove("show-menu");
+    document.body.style.overflow = "";
   });
 }
 
