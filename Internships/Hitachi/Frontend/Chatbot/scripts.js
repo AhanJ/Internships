919cf903864b2cd42c86fbd38b35d8470f031c5e
Ahan Jain (PC) <ahanjain@gmail.com>
2024-06-18 12:14:47 +0530

    Updated chatbot ui

--- Internships/Hitachi/Frontend/Chatbot/scripts.js
+++ Internships/Hitachi/Frontend/Chatbot/scripts.js
@@ -6,7 +6,7 @@ const chatBox = document.getElementById("chatbox");
 
 // Show and Hide Bot
 botToggle.addEventListener("click", () => {
-  chatBot.classList.toggle("hide-bot");
+  chatBot.classList.toggle("show-bot");
 });
 
 // Change Colour of Send Button if No Input is Provided
@@ -23,7 +23,8 @@ userInput.addEventListener("input", () => {
 // Add Text to Chat on Clicking Send
 sendButton.addEventListener("click", () => {
   let p = document.createElement("p");
-  p.innerHTML = userInput.value;
+  const userMsg = userInput.value;
+  p.innerHTML = userMsg;
 
   let li = document.createElement("li");
   li.appendChild(p);
@@ -33,6 +34,21 @@ sendButton.addEventListener("click", () => {
   userInput.value = "";
   sendButton.classList.remove("enabled");
   sendButton.disabled = true;
+
+  // processInput is passed in a wrapper callback function which is executed after 600ms
+  // if we just passed the function as it is, it would call it immediately and return output (none in this case) after 600ms
+  setTimeout(() => {
+    processInput(userMsg);
+  }, 600);
 });
 
 // Function Which Sends User Input to Rasa API and
+function processInput(userMsg) {
+  let p = document.createElement("p");
+  p.innerHTML = userMsg;
+
+  let li = document.createElement("li");
+  li.appendChild(p);
+  li.classList.add("chat", "outgoing");
+  chatBox.appendChild(li);
+}
