d16b62d5307a6c2352dcca2857c0a8cea698ab4f
Ahan Jain (MBA) <110718911+AhanJ@users.noreply.github.com>
2025-02-05 01:17:42 +0530

    Update printN.cpp

--- Vidyakul/Sandbox/Misc/CPP/printN.cpp
+++ Vidyakul/Sandbox/Misc/CPP/printN.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-void printN (int n)
+void printN(int n)
 
 {
     char A[n][2 * n]; // [n][n]
-    int start,end,i,j,k,mid;
+    int start, end, i, j, k, mid;
 
-    for(i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
 
     {
         for (j = 0; j < 2 * n; j++)
@@ -18,9 +18,9 @@ void printN (int n)
             // {
             //     A[i][j] = ' ';
             // }
-            
-            if (j == 0 || j == 2 * n - 2 || (2 * i == j) && (i != n-1)) // j == n - 1
-            
+
+            if (j == 0 || j == 2 * n - 2 || (2 * i == j) && (i != n - 1)) // j == n - 1
+
             {
                 A[i][j] = '*';
             }
@@ -32,9 +32,9 @@ void printN (int n)
 
             //     {
             //         A[i][j] = '*'
-            //     }                
+            //     }
             // }
-            
+
             else
 
             {
@@ -43,7 +43,7 @@ void printN (int n)
         }
     }
 
-    for(i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
 
     {
         for (j = 0; j < 2 * n; j++)
@@ -60,7 +60,7 @@ int main()
 
 {
     int n;
-    
+
     cout << "Enter the Height of the Letters: ";
     cin >> n;
 
